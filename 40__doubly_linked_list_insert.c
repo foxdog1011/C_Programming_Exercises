afd4157e0d29613d8f55e74b1d3a126e9662f20f
foxdog1011 <eason5168@me.com>
2024-10-10 23:08:53 +0800

    updated

---
+++ 40__doubly_linked_list_insert.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node{
+    int data;
+    struct Node* prev;
+    struct Node* next;
+};
+
+struct Node* createNode(int data){
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = data;
+    newNode->prev = NULL;
+    newNode->next = NULL;
+    return newNode;
+}
+
+void insertAtPosition(struct Node** head, int data, int position ){
+    struct Node* newNode = createNode(data);
+    if (position==1){
+        newNode->next = *head;
+        if (*head !=NULL){
+            (*head)->prev = newNode;
+        }
+        *head = newNode;
+        return;
+    }
+    struct Node* temp = *head;
+    for (int i=1; temp != NULL && i <position -1;i++){
+        temp = temp->next;
+    };
+
+    if (temp==NULL){
+        printf("Position out of range\n");
+        return;
+    }
+    newNode->next = temp->next;
+    if(temp->next != NULL){
+        temp->next->prev = newNode;
+    }
+    temp ->next = newNode;
+    newNode->prev = temp;
+}
+
+void printList(struct Node* node){
+    while(node != NULL){
+        printf("%d", node->data );
+        node = node->next;
+    }
+    printf("\n");
+}
+
+int main(){
+    struct Node* head = NULL;
+
+    insertAtPosition(&head,10,1);
+    insertAtPosition(&head,20,2);
+    insertAtPosition(&head,30,3);
+
+    printf("鍊表內容:");
+    printList(head);
+    return 0;
+}
---
+++ 41_doubly_linked_list_reverse.c
@@ -0,0 +1,68 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node{
+    int data;
+    struct Node* prev;
+    struct Node* next;
+};
+
+struct Node* createNode(int data){
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = data;
+    newNode->prev = NULL;
+    newNode->next= NULL;
+    return newNode;
+}
+
+void append(struct Node** head, int data){
+    struct Node* newNode = createNode(data);
+    if (*head ==NULL){
+        *head = newNode;
+        return ;
+    }
+    struct Node* temp = *head;
+    while(temp->next != NULL){
+        temp = temp->next;
+    }
+    temp->next = newNode;
+    newNode->prev = temp;
+}
+
+void reverse(struct Node** head){
+    struct Node* temp = NULL;
+    struct Node* current = *head;
+    while(current != NULL){
+        temp = current->prev;
+        current->prev = current->next;
+        current->next = temp;
+        current = current->prev;
+    }
+    if (temp != NULL){
+        *head = temp->prev;
+    }
+}
+
+void printList(struct Node* node){
+    while (node != NULL){
+        printf("%d", node ->data);
+        node = node->next;
+    }
+    printf("\n");
+}
+
+int main(){
+    struct Node* head = NULL;
+
+    append(&head,10);
+    append(&head,20);
+    append(&head,30);
+
+    printf("初始鍊表:");
+    printList(head);
+
+    reverse(&head);
+    printf("反轉鍊表:");
+    printList(head);
+    return 0;
+}
